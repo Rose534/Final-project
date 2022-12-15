d805b2e5d4e14f25f8c87b63b6cdfbae74411e07
rose534 <wambuirose534@gmail.com>
2022-12-15 14:58:07 +0300

    login btn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-let loginPage = document.querySelector(".login");
-
 function hidelogin() {
-    loginPage.style.display = "none";
+    document.getElementById("leftbar").style.display="none";
+    document.getElementById("logButton").style.display="inline";
 }
