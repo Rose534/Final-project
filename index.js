d957bea84ded072c1c65a0d9c2a55e699749e6f1
rose534 <wambuirose534@gmail.com>
2022-12-15 23:31:06 +0300

    fetch

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-//login page
-
-const btn = document.getElementById("logButton")
-const show = document.getElementById("leftbar")
-const login = document.getElementById("login")
 
 
-
-btn.addEventListener('click', (e) => { 
-    login.style.display = "none"
-    e.preventDefault();
-     
-   
-})
