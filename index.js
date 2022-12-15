5db22ff089e6a2de12603d2f0ab725223905b971
rose534 <wambuirose534@gmail.com>
2022-12-15 15:27:13 +0300

    login page

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 //login page
+const btn = document.getElementById("logButton")
+const show = document.getElementById("leftbar")
+const show2 = document.getElementById("searchbar")
 
-
+btn.addEventListener('click', () => {
+    if (btn === 'click') {
+         show = style.display = block; 
+    }
+})
