01af418c9c7341db09d9dcf385f6d4d6668b0dee
rose534 <wambuirose534@gmail.com>
2022-12-15 18:00:13 +0300

    login page

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 //login page
+
 const btn = document.getElementById("logButton")
 const show = document.getElementById("leftbar")
-const show2 = document.getElementById("searchbar")
 const login = document.getElementById("login")
-const everything = document.getElementById("everything")
+
+
 
 btn.addEventListener('click', () => { 
     login.style.display = "none"
+    if (login.style.display === "none") {
+        delete login;
+    }
+    
+    
 })
