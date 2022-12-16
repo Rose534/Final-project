b22016b51d34453d7ca17ba5e31d5757e0aaabca
rose534 <wambuirose534@gmail.com>
2022-12-16 21:49:05 +0300

    search api

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-const popularMovies = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=73767b2552b171f42316e0608f521656";
+const Movies = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=73767b2552b171f42316e0608f521656";
 
 const API_KEY = "73767b2552b171f42316e0608f521656";
 
 const https = "https://api.themoviedb.org/3";
 
-const image = "https://image.tmdb.org/t/p/original/";
+const image_Path = "https://image.tmdb.org/t/p/original/";
+
+const search_url = "https://api.themoviedb.org/3/search/movie?api_key=73767b2552b171f42316e0608f521656&query=\""
 
 //Search feature
 
-const input = document.querySelector(".searchbar")
-const btn = document.querySelector(".search")
-const maingridtitle = document.querySelector(".Watchlist h5")
-const maingrid = document.querySelector(".Watchlist .movie-grid")
+const form = document.getElementById('searchbar')
+const search = document.getElementById('Search')
+const main = document.getElementById('movie-grid')
 
-
-async function searchmovie(searchTerm) {
-   const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm}`)
-   const respData = await resp.json()
-   
-   return respData.results
+async function getMovies(url){
+const res = await fetch(url)
+const data = res.json()
+console.log(data.results);
 }
 
-btn.addEventListener('click', searched_movies)
-
-async function searched_movies () {
-    const data = await searchmovie (input) 
-    console.log(data);    
-
-    maingridtitle.innerHTML = `search Results...`
-    maingrid.innerHTML = data.map(e => {
-        return ``
-    })
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const searchValue = search.value 
+    if(searchValue && searchValue !=='') {
+        Movies(search_url+searchValue)
     }
+})
