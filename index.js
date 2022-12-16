1c0eae365da6a97585cf5e3d06a58e9a5e0ffe21
rose534 <wambuirose534@gmail.com>
2022-12-16 15:51:01 +0300

    api key

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const popularMovies = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=73767b2552b171f42316e0608f521656";
 
+const TMBDkey = "73767b2552b171f42316e0608f521656";
 
+const https = "https://api.themoviedb.org/3";
+
+const image = "https://image.tmdb.org/t/p/original/"
+
+const input = document.querySelector(".searchbar")
+const btn = document.querySelector(".search")
