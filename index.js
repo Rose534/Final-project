de911d46acc5a41ec3c43abc23098a67a8c8b6f3
rose534 <wambuirose534@gmail.com>
2022-12-16 16:46:06 +0300

    search bar

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const https = "https://api.themoviedb.org/3";
 
 const image = "https://image.tmdb.org/t/p/original/"
 
-
+//Search feature
 
 const input = document.querySelector(".searchbar")
 const btn = document.querySelector(".search")
 const maingridtitle = document.querySelector(".Watchlist h5")
 const maingrid = document.querySelector(".Watchlist .movie-grid")
 
-searchmovie('game')
+
 async function searchmovie(searchTerm) {
    const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm}`)
    const respData = await resp.json()
-   console.log(respData)
+   
    return respData.results
 }
 
-btn.addEventListener('click', add_searched_movies_to_dom)
+btn.addEventListener('click', searched_movies)
 
-async function add_searched_movies_to_dom () {
-    const data = await searchmovie (input.value) 
+async function searched_movies () {
+    const data = await searchmovie (input) 
     console.log(data);    
     }
